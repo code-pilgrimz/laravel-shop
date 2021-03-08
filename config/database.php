42c85bb122680d67f4c9acc6e478e91c86cef767
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2021-03-08 21:18:52 +0900

    database config

---
+++ config/database.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'default' => env('DB_CONNECTION', 'pgsql'),
+    'connections' => [
+        'pgsql' => [
+            'driver' => 'pgsql',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'database' => env('DB_DATABASE', 'app'),
+        ],
+    ],
+];
