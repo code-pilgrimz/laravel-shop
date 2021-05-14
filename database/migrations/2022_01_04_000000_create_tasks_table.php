fd25bc8a7e2e78356cc3d8423657ee0fee96d662
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2021-05-14 16:41:53 +0900

    task migration

---
+++ database/migrations/2022_01_04_000000_create_tasks_table.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->nullable();
+            $table->integer('priority')->nullable();
+            $table->boolean('done')->nullable();
+            $table->string('due_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void { Schema::dropIfExists('tasks'); }
+};
