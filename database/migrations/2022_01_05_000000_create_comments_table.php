d3865e4b04e34d11e4174cf84b26b8468e9a9242
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2021-05-28 10:37:02 +0900

    comment migration

---
+++ database/migrations/2022_01_05_000000_create_comments_table.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->string('body')->nullable();
+            $table->integer('author_id')->nullable();
+            $table->boolean('edited')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void { Schema::dropIfExists('comments'); }
+};
