67a089595130117284a7150e24d9332b2933215a
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2021-05-29 17:06:39 +0900

    comment feature test

---
+++ tests/Feature/CommentTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class CommentTest extends TestCase
+{
+    public function test_index_returns_ok(): void
+    {
+        $this->getJson('/api/comments')->assertStatus(200);
+    }
+}
