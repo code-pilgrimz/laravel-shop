7a5c30e7a071e6d303b255bae1eb471456a4e07a
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2021-09-10 15:02:39 +0900

    webhook feature test

---
+++ tests/Feature/WebhookTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class WebhookTest extends TestCase
+{
+    public function test_index_returns_ok(): void
+    {
+        $this->getJson('/api/webhooks')->assertStatus(200);
+    }
+}
