a4548cf0bde7d230843d75e5466bca5803c969b8
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2021-09-30 18:16:54 +0900

    audit_log migration

---
+++ database/migrations/2022_01_12_000000_create_audit_logs_table.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('audit_logs', function (Blueprint $table) {
+            $table->id();
+            $table->string('action')->nullable();
+            $table->integer('actor_id')->nullable();
+            $table->string('target')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void { Schema::dropIfExists('audit_logs'); }
+};
